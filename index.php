0255d5bb01109617db78c297c71802f1dbf35084
Fvtimv <fatima.hilmi@hotmail.com>
2023-02-08 11:05:05 +0100

    Finished Exercise 15 php 1 with OOP

--- index.php
+++ index.php
@@ -302,3 +302,62 @@ $diff = $dateNaissance->diff($dateCourante);
 echo "Age de la personne : $diff->y ans $diff->m mois $diff->d jours<br>";
 
 ?>
+
+
+<h1>Exercice 15</h1>
+
+<p>Créer une classe Personne (nom, prénom et date de naissance).
+Instancier 2 personnes et afficher leurs informations : nom, prénom et âge.
+$p1 = new Personne("DUPONT", "Michel", "1980-02-19") ;
+$p2 = new Personne("DUCHEMIN", "Alice", "1985-01-17") ;</p>
+
+<?php
+
+class Personne {
+    private $_nom;
+    private $_prenom;
+    private $_dateNaissance;
+
+    public function __construct($nom, $prenom, $dateNaissance){
+        $this -> _nom = $nom;
+        $this -> _prenom = $prenom;
+        $this -> _dateNaissance = $dateNaissance;
+    }
+    
+    public function getNom() //voici ce que l'on appele un "gettere
+    {
+        return $this -> _nom ;
+    }
+    public function getPrenom()
+    {
+        return $this -> _prenom ;
+    }
+    // public function getDateNaissance()
+    // {
+    //     return $this -> _dateNaissance ;
+    // }
+    public function calcAge(){
+        $dateNaissance = new DateTime($this -> _dateNaissance);
+        $dateCourante = new DateTime();
+        $diff = $dateNaissance -> diff($dateCourante);
+        return $diff -> y;
+    }
+
+    public function displayInformation()
+    {
+        // $this -> _prenom ;
+        // $this -> _nom ;
+        // $this -> calcAge ;
+        // echo "$this -> prenom." ".$this -> nom. " ".a" ".$this -> calcAge()." ".ans" ; faux demande
+        echo "{$this -> getPrenom()} {$this -> getNom()} a {$this -> calcAge()} ans <br>";
+    }
+    public function getDisplayInformation(){
+        return $this -> displayInformation();
+    }
+}
+
+$p1 = new Personne("DUPONT", "Michel", "1980-02-19");
+$p2 = new Personne("DUCHEMIN", "Alice", "1985-01-17");
+    
+echo $p1 -> getDisplayInformation();
+echo $p2 -> getDisplayInformation();
